b4dd02d0f630b79762c083a8506b7f48142f37ec
Odohi David <odohi.david@gmail.com>
2020-02-15 11:18:25 +0100

    followed justforfunc tutorial to implement the context package

--- mycontext/context/context.go
+++ mycontext/context/context.go
@@ -2,6 +2,7 @@ package contextimpl
 
 import (
 	"errors"
+	"sync"
 	"time"
 )
 
@@ -45,22 +46,19 @@ func TODO() Context {
 }
 
 type cancelCtx struct {
-	parent Context
-	done   chan struct{}
-	err    error
+	Context
+	done chan struct{}
+	err  error
+	mu   sync.Mutex
 }
 
-func (ctx *cancelCtx) Deadline() (deadline time.Time, ok bool) {
-	return ctx.parent.Deadline()
-}
 func (ctx *cancelCtx) Done() <-chan struct{} {
 	return ctx.done
 }
 func (ctx *cancelCtx) Err() error {
+	ctx.mu.Lock()
+	defer ctx.mu.Unlock()
 	return ctx.err
-}
-func (ctx *cancelCtx) Value(key interface{}) interface{} {
-	return ctx.parent.Value(key)
 }
 
 var Canceled = errors.New("context canceled")
@@ -69,14 +67,76 @@ type CancelFunc func()
 
 func WithCancel(parent Context) (Context, CancelFunc) {
 	ctx := &cancelCtx{
-		parent: parent,
-		done:   make(chan struct{}),
+		Context: parent,
+		done:    make(chan struct{}),
 	}
 
 	cancel := func() {
-		ctx.err = Canceled
-		close(ctx.done)
+		ctx.cancel(Canceled)
 	}
 
+	go func() {
+		select {
+		case <-parent.Done():
+			ctx.cancel(parent.Err())
+		case <-ctx.Done():
+		}
+	}()
+
 	return ctx, cancel
+}
+
+func (ctx *cancelCtx) cancel(err error) {
+	ctx.mu.Lock()
+	defer ctx.mu.Unlock()
+	// stop here if err != nil to prevent closing
+	// a closed channel. this can occur if the cancel
+	// function is called twice or the parent cancel is
+	// called and the current cancel is also called
+	if ctx.err != nil {
+		return
+	}
+	ctx.err = err
+	close(ctx.done)
+}
+
+type deadlineCtx struct {
+	*cancelCtx
+	deadline time.Time
+}
+
+func (ctx *deadlineCtx) Deadline() (deadline time.Time, ok bool) {
+	return ctx.deadline, true
+}
+
+var DeadlineExceeded = errors.New("deadline exceeded")
+
+func WithDeadline(parent Context, deadline time.Time) (Context, CancelFunc) {
+	cctx, cancel := WithCancel(parent)
+
+	ctx := &deadlineCtx{
+		cancelCtx: cctx.(*cancelCtx),
+		deadline:  deadline,
+	}
+
+	timer := time.AfterFunc(time.Until(deadline), func() {
+		ctx.cancel(DeadlineExceeded)
+	})
+
+	stop := func() {
+		timer.Stop()
+		cancel()
+	}
+
+	return ctx, stop
+}
+
+func WithTimeout(parent Context, timeout time.Duration) (Context, CancelFunc) {
+	return WithDeadline(parent, time.Now().Add(timeout))
+}
+
+func WithValue(parent Context, key interface{}, value interface{}) {
+	return &valueCtx{
+		Context: parent,
+	}
 }
--- mycontext/context/context_test.go
+++ mycontext/context/context_test.go
@@ -2,7 +2,9 @@ package contextimpl
 
 import (
 	"fmt"
+	"math"
 	"testing"
+	"time"
 )
 
 func TestBackgroundNotTODO(t *testing.T) {
@@ -25,5 +27,117 @@ func TestWithCancel(t *testing.T) {
 	<-ctx.Done()
 	if err := ctx.Err(); err != Canceled {
 		t.Errorf("Error should be equal to canceled: %v", err)
+	}
+}
+
+func TestWithCancelConcurrent(t *testing.T) {
+	ctx, cancel := WithCancel(Background())
+	time.AfterFunc(1*time.Second, cancel)
+
+	if err := ctx.Err(); err != nil {
+		t.Errorf("error should be nil first, got %v", err)
+	}
+
+	<-ctx.Done()
+	if err := ctx.Err(); err != Canceled {
+		t.Errorf("Error should be equal to canceled: %v", err)
+	}
+}
+
+func TestWithCancelPropagation(t *testing.T) {
+	ctxA, cancelA := WithCancel(Background())
+	ctxB, cancelB := WithCancel(ctxA)
+	defer cancelB()
+
+	cancelA()
+
+	select {
+	case <-ctxB.Done():
+	case <-time.After(1 * time.Second):
+		t.Error("time error")
+	}
+
+	if err := ctxB.Err(); err != Canceled {
+		t.Errorf("error should be canceled now, got %v", err)
+	}
+}
+
+func TestWithDeadline(t *testing.T) {
+	deadline := time.Now().Add(2 * time.Second)
+	ctx, cancel := WithDeadline(Background(), deadline)
+
+	if d, ok := ctx.Deadline(); !ok || d != deadline {
+		t.Errorf("expected deadline %v; got %v", deadline, d)
+	}
+
+	then := time.Now()
+	<-ctx.Done()
+	if d := time.Since(then); math.Abs(d.Seconds()-2.0) > 0.1 {
+		t.Errorf("should have been done after 2.0 seconds, took %v", d)
+	}
+	if err := ctx.Err(); err != DeadlineExceeded {
+		t.Errorf("error should be DeadlineExceeded, got %v", err)
+	}
+
+	cancel()
+	if err := ctx.Err(); err != DeadlineExceeded {
+		t.Errorf("error should still be DeadlineExceeded, got %v", err)
+	}
+}
+
+func TestWithTimeout(t *testing.T) {
+	timeout := 2 * time.Second
+	deadline := time.Now().Add(timeout)
+	ctx, cancel := WithTimeout(Background(), timeout)
+
+	if d, ok := ctx.Deadline(); !ok || d.Sub(deadline) > time.Millisecond {
+		t.Errorf("expected deadline %v; got %v", deadline, d)
+	}
+
+	then := time.Now()
+	<-ctx.Done()
+	if d := time.Since(then); math.Abs(d.Seconds()-2.0) > 0.1 {
+		t.Errorf("should have been done after 2.0 seconds, took %v", d)
+	}
+	if err := ctx.Err(); err != DeadlineExceeded {
+		t.Errorf("error should be DeadlineExceeded, got %v", err)
+	}
+
+	cancel()
+	if err := ctx.Err(); err != DeadlineExceeded {
+		t.Errorf("error should still be DeadlineExceeded, got %v", err)
+	}
+}
+
+func TestWithValue(t *testing.T) {
+	tc := []struct {
+		key, val, keyRet, valRet interface{}
+		shouldPanic              bool
+	}{
+		{"a", "b", "a", "b", false},
+		{"a", "b", "c", nil, false},
+		{42, true, 42, true, false},
+		{42, true, int64(42), nil, false},
+		{nil, true, nil, nil, true},
+		{[]int{1, 2, 3}, true, []int{1, 2, 3}, nil, true},
+	}
+
+	for _, tt := range tc {
+		var panicked interface{}
+		func() {
+			defer func() { panicked = recover() }()
+
+			ctx := WithValue(Background(), tt.key, tt.val)
+			if val := ctx.Value(tt.keyRet); val != tt.valRet {
+				t.Errorf("expected value %v, got %v", tt.valRet, val)
+			}
+		}()
+
+		if panicked != nil && !tt.shouldPanic {
+			t.Errorf("unexpected panic: %v", panicked)
+		}
+		if panicked == nil && tt.shouldPanic {
+			t.Errorf("expected panic, but didn't get it")
+		}
 	}
 }
