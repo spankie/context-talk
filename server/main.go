a9278eaaaef7e27698751f0452cd594865daeb51
Odohi David <odohi.david@gmail.com>
2020-02-15 11:18:44 +0100

    modify the server code

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,18 +13,31 @@ func main() {
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
 
+func DoSomethingForLong(ctx context.Context, done chan struct{}) {
+	t, ok := ctx.Value(1).(time.Duration)
+	if ok {
+		time.Sleep(t)
+	}
+	close(done)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	// we can actually get a context from the request
 	ctx := r.Context()
 	log.Printf("handler started\n")
 	defer log.Printf("handler ended\n")
 
+	type key int
+	waiting := make(chan struct{}, 1)
+	ctx = context.WithValue(ctx, key(1), 5*time.Second)
+	go DoSomethingForLong(ctx, waiting)
+
 	select {
 	case <-ctx.Done():
 		err := ctx.Err()
 		log.Println(err)
 		http.Error(w, ctx.Err().Error(), http.StatusInternalServerError)
-	case <-time.After(5 * time.Second):
+	case <-waiting:
 		fmt.Fprintln(w, "Hello gopher")
 	}
 }
